afe03d38c92d890f4b98a42fe4a116674c0659c2
John <Johnnyk737@gmail.com>
2021-02-15 23:10:42 +0000

    Implementing timer with start, stop, restart, handling state

--- lib/pages/timer/workout_timer.dart
+++ lib/pages/timer/workout_timer.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
-// import 'package:quiver/async.dart';
 import 'dart:async' ;
-import 'package:provider/provider.dart';
+import 'package:flutter/foundation.dart';
 
 /*
   Class to handle the timer display and functionality
 */
 class WorkoutTimer extends StatefulWidget {
-  WorkoutTimer({Key key, context, this.workout});
+  WorkoutTimer({
+    Key key, 
+    context, 
+    @required this.workout}) : assert(workout != null), 
+    super(key: key);
 
   // This will be a map of the settings.
   // Should include sets, rounds, worktime and resttime data
@@ -28,17 +31,11 @@ class _WorkoutTimerState extends State<WorkoutTimer> {
   int secondsOffset = 0;
   int minutesOffset = 0;
   int countDown = 00;
-  // String _seconds = '';
-  // String _minutes = '';
   int _sets;
   int _rounds;
   int _totalTime = 0;
   int _restTime;
   int _workTime;
-  int _originalTime;
-  // CountdownTimer countdownTimer;
-  int _current = 10;
-  int _start = 10;
   Timer _timer;
   bool _isButtonEnabled = true;
   bool _isWorking;
@@ -48,47 +45,28 @@ class _WorkoutTimerState extends State<WorkoutTimer> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    // _seconds = this.workout['workTime'].seconds.toString();
-    // _minutes = this.workout['workTime'].minutes.toString();
     _sets = this.workout['sets'] as int;
     _rounds = this.workout['rounds'] as int;
-    _restTime = this.workout['restTime'] as int;
+    _restTime = this.workout['restTime'].getTotalSeconds() as int;
     _workTime = this.workout['workTime'].getTotalSeconds() as int;
-    _originalTime = _workTime;
-    _totalTime = 65;
+    activityState = Activity.WORKOUT;
+    _totalTime = 5;
   }
-
-  /** 
-   * I need to be able to handle calling a timer for the work time, then rest time
-   * and then loop through when there is a new round, then a new set until we're done
-   * I also need to keep track of when the timer is working and when it's resting
-   */ 
-  // void startWorkout() {
-  // }
-
-// class WorkTime {
-  // int _totalTime;
-  // int _originalTime;
-  // Timer _timer;
-  // bool _isDone = false;
-
-  // WorkTime(int totalTime) {
-  //   this._totalTime = totalTime;
-  //   this._originalTime = totalTime;
-  // }
   
-
   /// switches between rest and work, decrements set and rounds, and starts timer
   void switchActivity() {
     if (activityState == Activity.REST) {
+      print('switching activity to workout');
       setState(() {
+        _timer = null;
         activityState = Activity.WORKOUT;
         _totalTime = this._workTime;
       });
     } else if (activityState == Activity.WORKOUT) {
+      print('switching activity to rest');
       setState(() {
+        _timer = null;
         this._rounds--;
         if (this._sets == 0 && this._rounds == 0) {
           activityState = Activity.DONE;
@@ -97,25 +75,17 @@ class _WorkoutTimerState extends State<WorkoutTimer> {
           this._rounds = this.workout['rounds'] as int;
           activityState = Activity.REST;
           _totalTime = this._restTime;
+        } else {
+          activityState = Activity.REST;
+          _totalTime = this._restTime;
         }
       });
     }
     startTimer();
   }
 
-  // int resetTime() {
-  //   if (activityState == Activity.REST) {
-  //     return this._restTime;
-  //   } else {
-  //     return this._workTime;
-  //   }
-  // }
-
   void startTimer() async {
     // Disable the start button on click because there is a delay between button press and _timer.isActive
-    // setState(() {
-    //   _isButtonEnabled = false;
-    // });
     print('Timer started');
 
     _timer = Timer.periodic(
@@ -125,12 +95,9 @@ class _WorkoutTimerState extends State<WorkoutTimer> {
           print('Done');
           setState(() {
             timer.cancel();
-            // _isDone = true;
-            this.switchActivity();
-            if (this._sets == 0)
-            restartTimer();
-            // I need to be able to handle re-creating the timer to start the rest period
           });
+          this.switchActivity();
+          // restartTimer();
         } else {
           setState(() {
             _totalTime--;
@@ -140,21 +107,20 @@ class _WorkoutTimerState extends State<WorkoutTimer> {
   }
 
   /// This method will serve to pause the timer
-  ///
   void stopTimer() async {
     print('Timer paused');
-    // setState(() {
-    //   _isButtonEnabled = true;
-    // });
     _timer.cancel();
     _timer = null;
-    // _isDone = true;
+    setState(() {
+    });
   }
 
   void restartTimer() async {
     print('Timer restarted');
     setState(() {
       _totalTime = this._workTime;
+      _sets = this.workout['sets'] as int;
+      _rounds = this.workout['rounds'] as int;
       // _isButtonEnabled = true;
         
       if (_timer != null) {
@@ -171,26 +137,45 @@ class _WorkoutTimerState extends State<WorkoutTimer> {
     return '${min.toString().padLeft(2, '0')}:${sec.toString().padLeft(2, '0')}';
   }
 
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Container(
-            height: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(this.getDisplay(), 
-                  style: TextStyle(fontSize: 64),
+        child: Container(
+            height: 400,
+            padding: EdgeInsets.only(top: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget> [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text('Sets: $_sets',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    Text('Rounds: $_rounds',
+                      style: TextStyle(fontSize: 24),
+                    )
+                  ],
+                ),
+                Column(
+                  
+                  children: [
+                    Center(
+                      child: Text(this.activityState != null ? describeEnum(this.activityState) : 'WORKOUT',
+                        style: TextStyle(fontSize: 24),
+                      )
+                    ),
+                    Center(
+                      child: Text(this.getDisplay(), 
+                        style: TextStyle(fontSize: 64),
+                      ),
+                    )
+                  ],
                 )
               ],
             )
           )
-        )
       ),
       bottomNavigationBar: Container(
         child: Row(
@@ -254,77 +239,3 @@ class _WorkoutTimerState extends State<WorkoutTimer> {
     );
   }
 }
-
-// class WorkTime {
-//   int _totalTime;
-//   int _originalTime;
-//   Timer _timer;
-//   bool _isDone = false;
-
-//   WorkTime(int totalTime) {
-//     this._totalTime = totalTime;
-//     this._originalTime = totalTime;
-//   }
-
-//   int get totalTime => _totalTime;
-
-//   bool get isDone => _isDone;
-
-//   void startTimer() async {
-//     // Disable the start button on click because there is a delay between button press and _timer.isActive
-//     // setState(() {
-//     //   _isButtonEnabled = false;
-//     // });
-//     print('Timer started');
-
-//     _timer = Timer.periodic(
-//       Duration(seconds: 1), 
-//       (Timer timer) { 
-//         if (_totalTime == 0) {
-//           print('Done');
-//           // setState(() {
-//             timer.cancel();
-//             _isDone = true;
-//             restartTimer();
-//             // I need to be able to handle re-creating the timer to start the rest period
-//           // });
-//         } else {
-//           // setState(() {
-//             _totalTime--;
-//           // });
-//         }
-//       });
-//   }
-
-//   /// This method will serve to pause the timer
-//   ///
-//   void stopTimer() async {
-//     print('Timer paused');
-//     // setState(() {
-//     //   _isButtonEnabled = true;
-//     // });
-//     _timer.cancel();
-//     _timer = null;
-//     _isDone = true;
-//   }
-
-//   void restartTimer() async {
-//     print('Timer restarted');
-//     // setState(() {
-//       _totalTime =_originalTime;
-//       // _isButtonEnabled = true;
-        
-//       if (_timer != null) {
-//         _timer.cancel();
-//         _timer = null;
-//       }
-//     // });
-//   }
-
-//   String getDisplay() {
-//     var min = (_totalTime / 60).floor();
-//     var sec = (_totalTime % 60);
-
-//     return '${min.toString().padLeft(2, '0')}:${sec.toString().padLeft(2, '0')}';
-//   }
-// }
